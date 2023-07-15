859e6a29188cc5a8101b5cb3f18c517af96f9ea0
Noah-Fox <nf238220@ohio.edu>
2023-07-15 15:03:50 -0400

    graph problem

---
+++ completed/cses-graphs/longest-flight-route.cc
@@ -0,0 +1,76 @@
+//https://cses.fi/problemset/task/1680
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// #include <stack>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void topoSort(vector<vector<ll>>& graph, stack<ll>& sorted, ll onNode, vector<ll>& states){
+    if (states[onNode] == 2) return;
+    states[onNode] = 1;
+    for (auto it = graph[onNode].begin(); it != graph[onNode].end(); it ++){
+        topoSort(graph,sorted,*it,states);
+    }
+    states[onNode] = 2;
+    sorted.push(onNode);
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+
+    ll nodeAmount, edgeAmount;
+    cin >> nodeAmount >> edgeAmount;
+
+    //input graph
+    vector<vector<ll>> graph(nodeAmount+1);
+    ll a, b;
+    for (ll i = 0; i < edgeAmount; i ++){
+        cin >> a >> b;
+        graph[a].push_back(b);
+    }
+
+    //get topological sorting of graph
+    vector<ll> states(nodeAmount+1,0);
+    stack<ll> sorted;
+    topoSort(graph, sorted, 1, states);
+    
+    //solve
+    vector<ll> dist(nodeAmount+1,0);
+    vector<ll> fromNode(nodeAmount+1,0);
+    dist[1] = 1;
+    while (!sorted.empty()){
+        ll onNode = sorted.top();
+        sorted.pop();
+        for (auto it = graph[onNode].begin(); it != graph[onNode].end(); it ++){
+            if (dist[onNode] + 1 > dist[*it]){
+                dist[*it] = dist[onNode]+1;
+                fromNode[*it] = onNode;
+            }
+        }
+    }
+
+    //output solution
+    if (dist[nodeAmount] == 0){
+        cout << "IMPOSSIBLE\n";
+    }
+    else {
+        stack<ll> path;
+        path.push(nodeAmount);
+        while (fromNode[path.top()] != 0){
+            path.push(fromNode[path.top()]);
+        }
+        cout << dist[nodeAmount] << "\n";
+        while (!path.empty()){
+            cout << path.top() << " ";
+            path.pop();
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
